34ae2a87b6e1660a994ae75167ba778cb93e277b
Joseph <JosephSped23@gmail.com>
2024-03-20 17:07:27 -0400

    completed adding the different components to the app

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+import "../index.css"
 
 function App() {
   return (
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { name, city } from "../data/data.js";
+import { name as myName, city as myCity } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id = "home">
+    <h1 style={{color: "firebrick"}}> "{myName} is a Web Developer from {myCity}"</h1>
+  </div>;
 }
 
 export default Home;
